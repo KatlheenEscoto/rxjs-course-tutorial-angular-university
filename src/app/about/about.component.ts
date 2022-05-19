4d84b1e4fda3dc51a8ef9c683cd9bda14f378a95
KE20815 <katlheen.escoto@bancocuscatlan.com>
2022-05-19 15:17:08 -0600

    Create an observable from a promise and use a map operator.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: 'about',
@@ -10,6 +13,25 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const http$ = createHttpObservable('/api/courses');
+    const courses$ =  http$.pipe(
+      map( response => Object.values(response['payload']) )
+    );
+
+    http$.subscribe(
+      courses => { console.log(courses) },
+      () => {}, // No handling error.
+      () => { console.log('Complete') }
+    );
+      
+    courses$.subscribe(
+        courses => { console.log(courses) },
+        () => {}, // No handling error.
+        () => { console.log('Complete') }
+    );
+
+    
+
   }
 
 }
