ae29d2dbe3f568821cd15f324350a71d04276cbb
KE20815 <katlheen.escoto@bancocuscatlan.com>
2022-05-30 11:00:00 -0600

    Use custom rxjs operator.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -17,6 +17,7 @@ import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { searchLessons } from '../../../server/search-lessons.route';
+import { debug, RxJsLoggingLevel } from '../common/debug';
 
 
 @Component({
@@ -38,7 +39,10 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+            .pipe(
+                debug(RxJsLoggingLevel.INFO, "course value "),
+            );
     }
     
     ngAfterViewInit() {
@@ -46,9 +50,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
         .pipe(
             map( event => event.target.value),
             startWith(''),
+            debug(RxJsLoggingLevel.INFO, "search "),
             debounceTime(400),
             distinctUntilChanged(), 
-            switchMap( search => this.loadLessons(search) )
+            switchMap( search => this.loadLessons(search) ),
+            debug(RxJsLoggingLevel.INFO, "lessons value "),
         )
 
     }
